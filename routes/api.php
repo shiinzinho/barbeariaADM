57dfc4e134a1e22eceeb130055f7d0f09f7b48f1
shiinzinho <gabrieldosantoslacerda@gmail.com>
2024-02-01 10:15:40 -0300

    UPDATE

--- routes/api.php
+++ routes/api.php
@@ -125,9 +125,43 @@ Route::delete('payment/delete/{id}', [PagamentoController::class, 'pagamentoExcl
 
 Route::post('professional/client', [ClienteController::class, 'cliente']);
 
+Route::get('professional/client/find/{id}', [ClienteController::class, 'clienteId']);
+
+Route::post('professional/client/name', [ClienteController::class, 'clienteNome']);
+
+Route::post('professional/client/cpf', [ClienteController::class, 'clienteCpf']);
+
+Route::post('professional/client/cellphone', [ClienteController::class, 'clienteCelular']);
+
+Route::post('professional/client/email', [ClienteController::class, 'clienteEmail']);
+
+Route::get('professional/client/all', [ClienteController::class, 'clienteRetornar']);
+
+Route::delete('professional/client/delete/{id}', [ClienteController::class, 'clienteExcluir']);
+
+Route::put('professional/client/update', [ClienteController::class, 'clienteUpdate']);
+
+Route::post('professional/client/restore', [ClienteController::class, 'clienteRestaurar']);
+
 //Agenda--Profissional
 
+Route::post('professional/schedule', [AgendaController::class, 'agenda']);
+
+Route::get('professional/schedule/all', [AgendaController::class, 'agendaRetornar']);
+
+Route::delete('professional/schedule/delete/{id}', [AgendaController::class, 'agendaExcluir']);
+
+Route::put('professional/schedule/update', [AgendaController::class, 'agendaUpdate']);
+
 Route::post('professional/schedule/time', [AgendaController::class, 'agendaTimeProfissional']);
+
+Route::post('professional/schedule/find', [AgendaController::class, 'agendaFind']);
+
+Route::post('professional/schedule/date', [AgendaController::class, 'agendaFindData']);
+
+Route::get('professional/schedule/find/{id}', [AgendaController::class, 'agendaId']);
+
+Route::post('professional/schedule/find/time/professional', [AgendaController::class, 'agendaFindTimeProfissional']);
 
 
 //------------------------------------------- ADMNISTRADOR -------------------------------------------//
